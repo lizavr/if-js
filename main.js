0f85011063c8481f89c99539fbf1cdd575f69cb6
lizavr <elizavetarabetskaya@gmail.com>
2022-12-16 21:38:16 +0300

    done tasks 1-7

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const text = 'Hello world!';
-console.log(text);
+let user = 'John Doe';
+console.log(user);
+const student = 'Liza';
+console.log(student);
+user = student; //'Liza'
+console.log(student);
+
+let test = 1;
+test += 1;
+test += '1'; //'21'
+console.log(test);
+test -= 1; //20
+console.log(test);
+test = !test; //false
+
+console.log(test);
